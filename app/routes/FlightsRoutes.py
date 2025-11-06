987954eb72768c51163766d13e4c6a5016b58859
CeciG24git commit -m primer <ceciliaga604@gmail.com>
2025-11-06 16:08:39 -0600

    add requirements.txt

--- app/routes/FlightsRoutes.py
+++ app/routes/FlightsRoutes.py
@@ -7,11 +7,17 @@ flights_bp = Blueprint('flights', __name__, url_prefix='/flights')
 
 @flights_bp.route("/update_flights", methods=["GET"])
 def update_flights():
-    flights = get_flights_over_cdmx()
-    count = save_flights(flights)
-    return jsonify({"message": f"{count} vuelos guardados correctamente."})
+    try:
+        flights = get_flights_over_cdmx()
+        count = save_flights(flights)
+        return jsonify({"message": f"{count} vuelos guardados correctamente."})
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 @flights_bp.route("/", methods=["GET"])
 def get_flights():
-    flights = get_all_flights()
-    return jsonify(flights)
+    try:
+        flights = get_all_flights()
+        return jsonify(flights)
+    except Exception as e:
+        return jsonify({"error": str(e)})
